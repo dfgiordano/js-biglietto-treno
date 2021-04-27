d6f4f1c1ab4bf66064f1c7c313b9bead78c6235a
dfgiordano <federico.giordano90@gmail.com>
2021-04-27 15:40:23 +0200

    esercizio terminato

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+var percorso = prompt("Quanti km devi percorerre?")
+console.log("Durata del viaggio: " + percorso + " km");
+var eta = prompt("Quanti anni hai?")
+console.log("Età del passeggero " + eta + " anni");
+
+var percorsoInNumero = parseInt(percorso);
+console.log(percorsoInNumero + " km");
+var etaInNumero = parseInt(eta);
+console.log(etaInNumero + " anni");
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+var prezzo = percorso * 0.21 ;
+console.log(prezzo + " € prezzo del biglietto");
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+if ( etaInNumero < 18 ) {
+    var costoFinale = prezzo * 80 / 100 ;
+    console.log(costoFinale);
+} if ( etaInNumero > 65 ) {
+    var costoFinale = prezzo * 60 / 100 ; 
+    console.log(costoFinale);
+} 
+
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+document.getElementById("costo_finale").innerHTML = costoFinale;
