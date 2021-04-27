e494dba0e70b461af3dc9088f2639d0971d559f3
dfgiordano <federico.giordano90@gmail.com>
2021-04-27 17:07:25 +0200

    manca decimali e intervallo medio

--- js/script.js
+++ js/script.js
@@ -9,19 +9,26 @@ console.log(percorsoInNumero + " km");
 var etaInNumero = parseInt(eta);
 console.log(etaInNumero + " anni");
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-
 var prezzo = percorso * 0.21 ;
 console.log(prezzo + " € prezzo del biglietto");
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
-
 if ( etaInNumero < 18 ) {
     var costoFinale = prezzo * 80 / 100 ;
-    console.log(costoFinale);
-} if ( etaInNumero > 65 ) {
+    console.log(costoFinale + " €");
+} else if ( etaInNumero > 65 ) {
     var costoFinale = prezzo * 60 / 100 ; 
-    console.log(costoFinale);
-} 
+    console.log(costoFinale + " €"); 
+} else {
 
+}
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-document.getElementById("costo_finale").innerHTML = costoFinale;
+//convertire in decimale
+//var costoFinale = parseDecimal(prezzo);
+
+//output pagina
+document.getElementById("costo_finale").innerHTML = costoFinale + " € E' il costo del tuo biglietto!";
+
+
+// ( etaInNumero > 18, etaInNumero < 65 ) ;
+//     console.log(prezzo + " €");
